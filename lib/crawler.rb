e0f72ed89c3c2213215a7c35a0532c8a9c2a9b7b
Francis <francis.beauchampsc@gmail.com>
2016-02-24 17:38:29 -0500

    Syntactic sugar

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -1,19 +1,15 @@
 require 'mechanize'
 require './lib/request_builder.rb'
 
-def crawl(params)
-  
+def crawl(params)  
   mechanize = Mechanize.new
   
   for i in 1..100 
-
-    params[:page]=i  
+    params[:page] = i  
     url = build_url(params)
     page = mechanize.get(url)
     
-    puts "Starting search for " + url
-    page.search('td.description').each do |description|
-      p description.at('a.title').content
-    end
+    puts "Starting search for #{url}"
+    page.search('td.description').each { |desc| p desc.at('a.title').content }
   end
 end
--- lib/request_builder.rb
+++ lib/request_builder.rb
@@ -6,7 +6,7 @@ LOCATIONS = {
 
 def build_url(params)
   url = BASE_URL
-  url += "b-" + LOCATIONS[params[:location]] + "/" if params.has_key?(:location)
+  url += "b-#{LOCATIONS[params[:location]]}/" if params.has_key?(:location)
   url += params[:keywords].join("-") + "/" if params.has_key?(:keywords)
   url += "page-#{params[:page]}/" if params.has_key?(:page)
   url += "k0l#{params[:location]}" if params.has_key?(:location)
