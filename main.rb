ae47d66c385d4ff764fdc8edb75e52eaa737b4e7
Vincent Fournier <v.fournier11@gmail.com>
2016-03-24 14:53:35 -0400

    Add terminal parser

--- main.rb
+++ main.rb
@@ -1,9 +1,34 @@
 #!/usr/bin/ruby
 
 require './lib/crawler.rb'
+require 'ostruct'
+require 'optparse'
 
+options = OpenStruct.new
+options.location = 9001
+
+opt_parser = OptionParser.new do |opt|
+  opt.banner = "Usage: main.rb [options]"
+  opt.separator ""
+  opt.separator "Options"
+
+  opt.on("-k", "--keywords Keywords", Array,
+         "The keywords for the search separated by comma") do |keywords|
+    options.keywords = keywords
+  end
+
+  opt.on("-l", "--location [Locations]", OptionParser::DecimalInteger,
+         "The location of the search") do |loc|
+    options.location = loc
+  end
+
+  opt.on_tail("-h", "--help", "Show this message") do
+    puts opt
+    exit
+  end
+end.parse!
 
 Crawler.new.crawl({
-  :location => 9001,
-  :keywords => ['civic', '2014']
+  :location => options.location,
+  :keywords => options.keywords
 })
